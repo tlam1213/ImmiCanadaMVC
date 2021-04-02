ef27390f8f2014d8f9bcb20fab01b1b69846ac83
tlam <tlam@proserveit.com>
2021-04-02 16:26:26 +0700

    Commit

---
+++ ImmiCanada/Scripts/main-script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".main-menu .menu-item a").click(function () {
+        var currentItem = $(this);
+        //$(".main-menu .menu-item div[aria-expanded='false']").removeClass("show");
+        $(".main-menu .menu-item div[aria-expanded='false']").each(function () {
+            if ($(this).attr("id") != currentItem.find("+ div").attr("id"))
+                $(this).removeClass("show");
+        });
+       //$(this).find("+ div").addClass("show");
+    });
+});
