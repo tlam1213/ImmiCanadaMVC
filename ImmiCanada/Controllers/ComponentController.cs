80f9d25ff2a48490a87fdaa98ca49dc083425f67
tlam <tlam@proserveit.com>
2021-06-28 00:02:23 +0700

    Changed

---
+++ ImmiCanada/Controllers/ComponentController.cs
@@ -0,0 +1,24 @@
+﻿using ImmiCanada.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImmiCanada.Controllers
+{
+    public class ComponentController : Controller
+    {
+        private ImmiCanadaEntities db = new ImmiCanadaEntities();
+        // GET: Component
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Works(string ServiceId)
+        {
+            return PartialView();
+        }
+    }
+}
